c592dc7421cce627db12cdfb22cd88fc76989cde
tspeidel <tommasino@speidel.ca>
2018-05-06 11:41:09 -0600

    Gitignore

--- geoconference_2018_PPT_Graphs.R
+++ geoconference_2018_PPT_Graphs.R
@@ -47,3 +47,60 @@ g3 <- g3 + geom_segment(data=dat, aes(x=xmin, xend=xmax, y=middle, yend=middle),
 library(patchwork)
 
 g1+g2+g3
+
+
+
+##
+ggplot(hunt, aes(gross.pay, phi.h)) + 
+  geom_point(color = "coral2", alpha = 0.8, size = 6) +   
+  geom_smooth(method='lm', color = "grey40") +
+  xlab("Gross Pay") +
+  ylab("phi.h") +
+  theme_minimal(base_family = "Raleway", base_size = 50) +
+  theme(panel.background = element_rect(fill = "#FCFCFC", colour = NA),
+        plot.background = element_rect(fill = "#FCFCFC", colour = NA))
+
+
+##
+ggplot(Predict(f1, fun = bar),
+       colfill = "gray50",
+       adj.subtitle = FALSE,
+       layout = c(1,3),
+       addlayer = theme_minimal(base_family = "Raleway", base_size = 30) +
+         labs(y = "Production"))
+##
+heatmap <- Predict(f1, phi.h, pressure, position.cat, fun = bar, np = 90)
+heatmap <- as.data.frame(heatmap)
+
+ggplot(heatmap, aes(phi.h, pressure)) + 
+                   geom_tile(aes(fill = yhat), color = "white")  + 
+                   scale_fill_viridis(discrete = FALSE, option = "D") +
+                   facet_grid(.~position.cat) +
+                   labs(x = "\nphi.h",
+                        y = "Pressure",
+                        fill = "Predicted\nMean\nProduction",
+                        title = "Predicted Production",
+                        subtitle = "Predicted mean production as a function of pressure, phi.h and position",
+                        caption = "Insufficient data to estimate production when pressure is 15.") + 
+                   scale_x_continuous(breaks = seq(0, 160, 20)) +
+                   scale_y_continuous(breaks = seq(0, 20, 2)) +
+                   theme_minimal() +
+                   theme(plot.title = element_text(face = "bold", hjust = 0, vjust = 0.8, colour = "#3C3C3C", size = 40, family = "Raleway")) +
+                   theme(plot.subtitle = element_text(hjust = 0, vjust = 0.8, colour = "#3C3C3C", size = 20, family = "Raleway")) +
+                   theme(plot.caption = element_text(size = 16, family = "Raleway")) +
+                   theme(axis.title = element_text()) +
+                   theme(axis.title.y = element_text(size = 16, angle = 90, family = "Raleway")) +
+                   theme(axis.title.x = element_text(size = 16, angle = 0, family = "Raleway")) +
+                   theme(axis.text.x = element_text(size = 14, family = "Raleway", color = "black", margin = margin(r=0))) +
+                   theme(axis.text.y = element_text(size = 14, family = "Raleway", color = "black")) +
+                   theme(legend.text = element_text(size = 12, family = "Raleway")) +
+                   theme(legend.title = element_text(size = 13, family = "Raleway", hjust = 0.5)) +
+                   theme(legend.position = "right") +
+                   theme(axis.line = element_blank(), 
+                         axis.ticks.y = element_blank(),
+                         panel.grid.major = element_blank(),
+                         panel.grid.minor = element_blank(),
+                         panel.border = element_blank()) +
+                   guides(colour = guide_legend(ncol = 1)) +
+                   theme(legend.title.align=0) +
+                   theme(panel.background = element_rect(fill = "#FCFCFC", colour = NA), plot.background = element_rect(fill = "#FCFCFC", colour = NA))
